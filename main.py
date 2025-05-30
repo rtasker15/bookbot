7a551ae5dc2f9d7e0dce16bed59af49b3ca15599
rtasker15 <rtasker15@gmail.com>
2025-05-30 21:33:17 +0100

    added count_words()

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("greetings boots")
+def get_book_text():
+    path_to_file = "books/frankenstein.txt"
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return(file_contents)
+
+
+def main():
+    get_book_text()
+    count_words()
+
+
+def count_words():
+    raw_words = get_book_text()
+    word_list = raw_words.split()
+    word_count = len(word_list)
+    print(f"{word_count} words found in the document")
+
+main()
